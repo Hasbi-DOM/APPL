569c4057e02e86efc813df73950656ca215cf732
Hasbi-DOM <61396381+Hasbi-DOM@users.noreply.github.com>
2021-03-23 22:31:14 +0700

    Soal4W2
    push 1

---
+++ CopyingaFile/src/main/java/CopyFile.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CopyFile {
+    
+    public static void main(String[] args) throws IOException 
+    {
+        
+        BufferedReader br;
+        String line;
+        Scanner scan = new Scanner(System.in);
+        
+        String val = scan.nextLine();
+        
+        br = new BufferedReader(new FileReader("D:\\HASBI\\" + val));
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+}
