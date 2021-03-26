90c1d53949aa4e9e4623d0476ee8f17c6ee9c3d8
Hasbi-DOM <61396381+Hasbi-DOM@users.noreply.github.com>
2021-03-26 20:14:40 +0700

    Soal8W2
    push 3

--- ListingPrimeNumbers/src/main/java/PrimePanel.java
+++ ListingPrimeNumbers/src/main/java/PrimePanel.java
@@ -8,9 +8,9 @@
  *
  * @author ASUS
  */
-import java.awt.*; 
-import java.awt.event.*; 
-import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class PrimePanel extends JPanel {
 
@@ -32,7 +32,7 @@ public class PrimePanel extends JPanel {
         number = new JTextField(8);
         computeButton = new JButton("Click to see all primes up to your number!");
         primeList = new JTextArea(10, 30);
-     
+
         JScrollPane scrollableTextArea = new JScrollPane(primeList);
         computeButton.addActionListener(new ButtonListener());
         // Add the components to the panel 
@@ -43,10 +43,10 @@ public class PrimePanel extends JPanel {
         //add(primeList);
         setPreferredSize(new Dimension(400, 320));
         setBackground(Color.yellow);
-        
+
         scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         add(scrollableTextArea);
-          
+
     }
     // ***************************************************************** 
     // Represents a listener for the click of the button. 
@@ -58,47 +58,50 @@ public class PrimePanel extends JPanel {
         // button is clicked. 
         // ----------------------------------------------------------- 
 
+        @Override
         public void actionPerformed(ActionEvent event) {
-            
-            String textNum = number.getText();
-            int num = Integer.parseInt(textNum);
-            String ans = "";
-            int count = 0;
-            if (num < 2) {
-                ans = "There no primes less than " + num;
-            } else {
-                ans = " " + 2;
-                count++;
-                for (int i = 3; i <= num; i += 2) {
-                    boolean foundDivisor = false;
-                    int j = 3;
-                    try {
-                    while (j < i && !foundDivisor) {
-                        
-                        if (i % j == 0) {
-                            foundDivisor = true;
-                        } else {
-                            j++;
-                        }
-                    
-                    // Add i to the list if it is prime 
-                    if (j == i) {
-                        ans += " " + i;
-                        count++;
-                        if (count % 10 == 0) {
-                            ans += "\n";
-                        }
-                    }   
-                        }  
-                    } catch (NumberFormatException exc){
-                    System.out.println("Format Number is Wrong!");
-                    }
-            primeList.setText(ans);
-                    }
-                }
-            }
-        }
-}
-    
-    
+            try {
+                String textNum = "";
+                textNum = number.getText();
+                int num = Integer.parseInt(textNum);
+                int count = 0;
+                String ans = "";
 
+                if (num < 2) {
+                    ans = "There no primes less than " + num;
+                } else {
+                    ans = " " + 2;
+                    count++;
+                    for (int i = 3; i <= num; i += 2) {
+                        boolean foundDivisor = false;
+                        int j = 3;
+
+                        while (j < i && !foundDivisor) {
+
+                            if (i % j == 0) {
+                                foundDivisor = true;
+                            } else {
+                                j++;
+                            }
+
+                            // Add i to the list if it is prime 
+                            if (j == i) {
+                                ans += " " + i;
+                                count++;
+                                if (count % 10 == 0) {
+                                    ans += "\n";
+                                }
+                            }
+                        }
+                    }
+                } 
+                primeList.setText(ans);
+            } catch (NumberFormatException exc) {
+                JFrame message;
+                message = new JFrame();
+                JOptionPane.showMessageDialog(message, "Format Number is Wrong!");
+            }
+
+        }
+    }
+}
